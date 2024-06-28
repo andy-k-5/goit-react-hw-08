70aad9e3e6a91fab1c88a3213752e5781eba3724
andy-k-5 <148633731+andy-k-5@users.noreply.github.com>
2024-06-28 19:04:29 +0200

    update

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { contactReducer } from "./contactsSlice";
+import { filterReducer } from "./filtersSlice";
+
+const rootReducer = combineReducers({
+  contacts: contactReducer,
+  filters: filterReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+});
